dd522500694fe8c365290ec48ef0a77942b31af6
Luigi <luigismilzo@live.com>
2020-09-22 17:40:48 +0200

    -

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ const infoBox = document.querySelector('.Container');
 
 gsap.from( infoBox, {
     x: 400,
-    duration: .5,
-    ease: 'power1'
+    duration: 1,
+    ease: 'power1',
+    opacity: 0
 })
