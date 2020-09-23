94f7c64118acb62679cc978993266066d33eaa6b
Luigi <luigismilzo@live.com>
2020-09-23 17:58:42 +0200

    Latest section

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,17 @@
 const infoBox = document.querySelector('.Container');
+const cta = document.querySelector('.Cta');
 
 gsap.from( infoBox, {
     x: 400,
-    duration: 1,
-    ease: 'power1',
+    duration: .5,
+    ease: 'linear',
     opacity: 0
 })
+
+cta.onclick = () => {
+    gsap.to( window, {
+        duration: .5,
+        scrollTo: { y:'#Latest', offsetY: -10},
+        ease: 'power1'
+    })
+}
