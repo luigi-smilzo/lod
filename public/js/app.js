f22db1f8b2c770d1489d5ac51671dc1023b2c3b7
Luigi <luigismilzo@live.com>
2020-09-23 20:55:47 +0200

    Responsive layout

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,11 @@
 const infoBox = document.querySelector('.Container');
 const cta = document.querySelector('.Cta');
+const hamburger = document.querySelector('.MainHeader-hamburger i');
+const nav = document.querySelector('.MainHeader-hamburger .Nav');
+const navLinks = document.querySelector('.MainHeader-hamburger .Nav .Nav-links a');
+
+hamburger.addEventListener('click', toggleMenu);
+navLinks.addEventListener('click', toggleMenu);
 
 gsap.from( infoBox, {
     x: 400,
@@ -7,3 +13,8 @@ gsap.from( infoBox, {
     ease: 'linear',
     opacity: 0
 })
+
+function toggleMenu() {
+    nav.classList.toggle('Nav--isHidden');
+    hamburger.classList.item(1) == 'fa-bars' ? hamburger.classList.replace('fa-bars', 'fa-times') : hamburger.classList.replace('fa-times', 'fa-bars')
+}
